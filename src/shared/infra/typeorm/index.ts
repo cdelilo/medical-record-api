eee41d3398698a824529189b82378dd4ec401e34
Christian Moreira <christiandelilomoreira@gmail.com>
2026-08-08 16:08:32 -0300

    fix: correct MYSQL_PASSWD to MYSQL_PASSWORD in TypeORM connection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -12,7 +12,7 @@ const Connection = new DataSource({
   host: `${process.env.MYSQL_HOST}`,
   port: Number(process.env.MYSQL_PORT),
   username: `${process.env.MYSQL_USER}`,
-  password: `${process.env.MYSQL_PASSWD}`,
+  password: `${process.env.MYSQL_PASSWORD}`,
   database: `${process.env.MYSQL_DATABASE}`,
   entities: [`./${rootDir}/modules/**/infra/typeorm/entities/*.${ext}`],
   migrations: [`./${rootDir}/shared/infra/typeorm/migrations/*.${ext}`],
